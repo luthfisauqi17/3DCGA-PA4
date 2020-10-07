0bab9e4cc23d5a4822d5521d4cbaec1ac005ed14
luthfisauqi17 <40749167+luthfisauqi17@users.noreply.github.com>
2020-10-08 06:00:48 +0700

    GUI Completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,328 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.VRPxTextBox = new System.Windows.Forms.TextBox();
+            this.VRPyTextBox = new System.Windows.Forms.TextBox();
+            this.VRPzTextBox = new System.Windows.Forms.TextBox();
+            this.VPNxTextBox = new System.Windows.Forms.TextBox();
+            this.VPNyTextBox = new System.Windows.Forms.TextBox();
+            this.VPNzTextBox = new System.Windows.Forms.TextBox();
+            this.VUPxTextBox = new System.Windows.Forms.TextBox();
+            this.VUPyTextBox = new System.Windows.Forms.TextBox();
+            this.VUPzTextBox = new System.Windows.Forms.TextBox();
+            this.COPxTextBox = new System.Windows.Forms.TextBox();
+            this.COPyTextBox = new System.Windows.Forms.TextBox();
+            this.COPzTextBox = new System.Windows.Forms.TextBox();
+            this.windowMinTextBox = new System.Windows.Forms.TextBox();
+            this.windowMaxTextBox = new System.Windows.Forms.TextBox();
+            this.FPTextBox = new System.Windows.Forms.TextBox();
+            this.BPTextBox = new System.Windows.Forms.TextBox();
+            this.debugTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(418, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VRP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(418, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "VPN:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(418, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "VUP:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(418, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "COP:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(418, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Window size:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(418, 311);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "FP:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(447, 239);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Min:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(447, 271);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Max:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(418, 348);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 17);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "BP:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(556, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 49);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // VRPxTextBox
+            // 
+            this.VRPxTextBox.Location = new System.Drawing.Point(464, 12);
+            this.VRPxTextBox.Name = "VRPxTextBox";
+            this.VRPxTextBox.Size = new System.Drawing.Size(100, 22);
+            this.VRPxTextBox.TabIndex = 1;
+            // 
+            // VRPyTextBox
+            // 
+            this.VRPyTextBox.Location = new System.Drawing.Point(572, 12);
+            this.VRPyTextBox.Name = "VRPyTextBox";
+            this.VRPyTextBox.Size = new System.Drawing.Size(100, 22);
+            this.VRPyTextBox.TabIndex = 2;
+            // 
+            // VRPzTextBox
+            // 
+            this.VRPzTextBox.Location = new System.Drawing.Point(678, 12);
+            this.VRPzTextBox.Name = "VRPzTextBox";
+            this.VRPzTextBox.Size = new System.Drawing.Size(100, 22);
+            this.VRPzTextBox.TabIndex = 3;
+            // 
+            // VPNxTextBox
+            // 
+            this.VPNxTextBox.Location = new System.Drawing.Point(464, 60);
+            this.VPNxTextBox.Name = "VPNxTextBox";
+            this.VPNxTextBox.Size = new System.Drawing.Size(100, 22);
+            this.VPNxTextBox.TabIndex = 4;
+            // 
+            // VPNyTextBox
+            // 
+            this.VPNyTextBox.Location = new System.Drawing.Point(572, 60);
+            this.VPNyTextBox.Name = "VPNyTextBox";
+            this.VPNyTextBox.Size = new System.Drawing.Size(100, 22);
+            this.VPNyTextBox.TabIndex = 5;
+            // 
+            // VPNzTextBox
+            // 
+            this.VPNzTextBox.Location = new System.Drawing.Point(678, 60);
+            this.VPNzTextBox.Name = "VPNzTextBox";
+            this.VPNzTextBox.Size = new System.Drawing.Size(100, 22);
+            this.VPNzTextBox.TabIndex = 6;
+            // 
+            // VUPxTextBox
+            // 
+            this.VUPxTextBox.Location = new System.Drawing.Point(464, 109);
+            this.VUPxTextBox.Name = "VUPxTextBox";
+            this.VUPxTextBox.Size = new System.Drawing.Size(100, 22);
+            this.VUPxTextBox.TabIndex = 7;
+            // 
+            // VUPyTextBox
+            // 
+            this.VUPyTextBox.Location = new System.Drawing.Point(572, 109);
+            this.VUPyTextBox.Name = "VUPyTextBox";
+            this.VUPyTextBox.Size = new System.Drawing.Size(100, 22);
+            this.VUPyTextBox.TabIndex = 8;
+            // 
+            // VUPzTextBox
+            // 
+            this.VUPzTextBox.Location = new System.Drawing.Point(678, 109);
+            this.VUPzTextBox.Name = "VUPzTextBox";
+            this.VUPzTextBox.Size = new System.Drawing.Size(100, 22);
+            this.VUPzTextBox.TabIndex = 9;
+            // 
+            // COPxTextBox
+            // 
+            this.COPxTextBox.Location = new System.Drawing.Point(464, 157);
+            this.COPxTextBox.Name = "COPxTextBox";
+            this.COPxTextBox.Size = new System.Drawing.Size(100, 22);
+            this.COPxTextBox.TabIndex = 10;
+            // 
+            // COPyTextBox
+            // 
+            this.COPyTextBox.Location = new System.Drawing.Point(572, 157);
+            this.COPyTextBox.Name = "COPyTextBox";
+            this.COPyTextBox.Size = new System.Drawing.Size(100, 22);
+            this.COPyTextBox.TabIndex = 11;
+            // 
+            // COPzTextBox
+            // 
+            this.COPzTextBox.Location = new System.Drawing.Point(678, 157);
+            this.COPzTextBox.Name = "COPzTextBox";
+            this.COPzTextBox.Size = new System.Drawing.Size(100, 22);
+            this.COPzTextBox.TabIndex = 12;
+            // 
+            // windowMinTextBox
+            // 
+            this.windowMinTextBox.Location = new System.Drawing.Point(490, 240);
+            this.windowMinTextBox.Name = "windowMinTextBox";
+            this.windowMinTextBox.Size = new System.Drawing.Size(100, 22);
+            this.windowMinTextBox.TabIndex = 13;
+            // 
+            // windowMaxTextBox
+            // 
+            this.windowMaxTextBox.Location = new System.Drawing.Point(490, 268);
+            this.windowMaxTextBox.Name = "windowMaxTextBox";
+            this.windowMaxTextBox.Size = new System.Drawing.Size(100, 22);
+            this.windowMaxTextBox.TabIndex = 14;
+            // 
+            // FPTextBox
+            // 
+            this.FPTextBox.Location = new System.Drawing.Point(453, 311);
+            this.FPTextBox.Name = "FPTextBox";
+            this.FPTextBox.Size = new System.Drawing.Size(100, 22);
+            this.FPTextBox.TabIndex = 15;
+            // 
+            // BPTextBox
+            // 
+            this.BPTextBox.Location = new System.Drawing.Point(453, 345);
+            this.BPTextBox.Name = "BPTextBox";
+            this.BPTextBox.Size = new System.Drawing.Size(100, 22);
+            this.BPTextBox.TabIndex = 16;
+            // 
+            // debugTextBox
+            // 
+            this.debugTextBox.Location = new System.Drawing.Point(795, 13);
+            this.debugTextBox.Multiline = true;
+            this.debugTextBox.Name = "debugTextBox";
+            this.debugTextBox.Size = new System.Drawing.Size(434, 425);
+            this.debugTextBox.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1241, 450);
+            this.Controls.Add(this.debugTextBox);
+            this.Controls.Add(this.BPTextBox);
+            this.Controls.Add(this.FPTextBox);
+            this.Controls.Add(this.windowMaxTextBox);
+            this.Controls.Add(this.windowMinTextBox);
+            this.Controls.Add(this.COPzTextBox);
+            this.Controls.Add(this.COPyTextBox);
+            this.Controls.Add(this.COPxTextBox);
+            this.Controls.Add(this.VUPzTextBox);
+            this.Controls.Add(this.VUPyTextBox);
+            this.Controls.Add(this.VUPxTextBox);
+            this.Controls.Add(this.VPNzTextBox);
+            this.Controls.Add(this.VPNyTextBox);
+            this.Controls.Add(this.VPNxTextBox);
+            this.Controls.Add(this.VRPzTextBox);
+            this.Controls.Add(this.VRPyTextBox);
+            this.Controls.Add(this.VRPxTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "3DCGA-PA4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox VRPxTextBox;
+        private System.Windows.Forms.TextBox VRPyTextBox;
+        private System.Windows.Forms.TextBox VRPzTextBox;
+        private System.Windows.Forms.TextBox VPNxTextBox;
+        private System.Windows.Forms.TextBox VPNyTextBox;
+        private System.Windows.Forms.TextBox VPNzTextBox;
+        private System.Windows.Forms.TextBox VUPxTextBox;
+        private System.Windows.Forms.TextBox VUPyTextBox;
+        private System.Windows.Forms.TextBox VUPzTextBox;
+        private System.Windows.Forms.TextBox COPxTextBox;
+        private System.Windows.Forms.TextBox COPyTextBox;
+        private System.Windows.Forms.TextBox COPzTextBox;
+        private System.Windows.Forms.TextBox windowMinTextBox;
+        private System.Windows.Forms.TextBox windowMaxTextBox;
+        private System.Windows.Forms.TextBox FPTextBox;
+        private System.Windows.Forms.TextBox BPTextBox;
+        private System.Windows.Forms.TextBox debugTextBox;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,37 @@ namespace _3DCGA_PA4
 {
     public partial class Form1 : Form
     {
+        //Define struct
+        public struct TPoint
+        {
+            public double x, y, z, w;
+        }
+
+        public struct TLine
+        {
+            public int p1, p2;
+        }
+
+
+        //Global variables
+        Bitmap bmp;
+        Graphics g;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Blue);
+
+            g.DrawLine(pen, new Point(0, pictureBox1.Height / 2), new Point(pictureBox1.Width, pictureBox1.Height / 2));
+            g.DrawLine(pen, new Point(pictureBox1.Width / 2, 0), new Point(pictureBox1.Width / 2, pictureBox1.Height));
+
+            pictureBox1.Image = bmp;
         }
     }
 }
